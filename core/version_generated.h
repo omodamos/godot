a4a86f1fe0db51b500836c23b0bf6025ae4fc2cf
Z R <arbantide@gmail.com>
2017-04-23 20:00:27 -0500

    revert

---
+++ core/version_generated.h
@@ -0,0 +1,7 @@
+#define VERSION_SHORT_NAME godot
+#define VERSION_NAME Godot Engine
+#define VERSION_MAJOR 3
+#define VERSION_MINOR 0
+#define VERSION_REVISION custom_build
+#define VERSION_STATUS alpha
+#define VERSION_YEAR 2017
