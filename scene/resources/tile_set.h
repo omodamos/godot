cfcf6eb763a5e3cc709add60cae99d139acbbe45
Z R <arbantide@gmail.com>
2017-06-28 19:35:17 -0700

    revert one-way tile maps (because we reverted one-way CollisionObject)

--- scene/resources/tile_set.h
+++ scene/resources/tile_set.h
@@ -40,26 +40,15 @@ class TileSet : public Resource {
 
 	GDCLASS(TileSet, Resource);
 
-public:
-	struct ShapeData {
-		Ref<Shape2D> shape;
-		Vector2 shape_offset;
-		bool one_way_collision;
-
-		ShapeData() {
-			one_way_collision = false;
-		}
-	};
-
-private:
-	struct TileData {
+	struct Data {
 
 		String name;
 		Ref<Texture> texture;
 		Ref<Texture> normal_map;
 		Vector2 offset;
+		Vector2 shape_offset;
 		Rect2i region;
-		Vector<ShapeData> shapes_data;
+		Vector<Ref<Shape2D> > shapes;
 		Vector2 occluder_offset;
 		Ref<OccluderPolygon2D> occluder;
 		Vector2 navigation_polygon_offset;
@@ -68,11 +57,11 @@ private:
 		Color modulate;
 
 		// Default modulate for back-compat
-		explicit TileData()
+		explicit Data()
 			: modulate(1, 1, 1) {}
 	};
 
-	Map<int, TileData> tile_map;
+	Map<int, Data> tile_map;
 
 protected:
 	bool _set(const StringName &p_name, const Variant &p_value);
@@ -99,24 +88,14 @@ public:
 	void tile_set_texture_offset(int p_id, const Vector2 &p_offset);
 	Vector2 tile_get_texture_offset(int p_id) const;
 
+	void tile_set_shape_offset(int p_id, const Vector2 &p_offset);
+	Vector2 tile_get_shape_offset(int p_id) const;
+
 	void tile_set_region(int p_id, const Rect2 &p_region);
 	Rect2 tile_get_region(int p_id) const;
 
-	void tile_set_shape(int p_id, int p_shape_id, const Ref<Shape2D> &p_shape);
-	Ref<Shape2D> tile_get_shape(int p_id, int p_shape_id) const;
-
-	void tile_set_shape_offset(int p_id, int p_shape_id, const Vector2 &p_offset);
-	Vector2 tile_get_shape_offset(int p_id, int p_shape_id) const;
-
-	void tile_set_shape_one_way(int p_id, int p_shape_id, bool p_one_way);
-	bool tile_get_shape_one_way(int p_id, int p_shape_id) const;
-
-	void tile_clear_shapes(int p_id);
-	void tile_add_shape(int p_id, const Ref<Shape2D> &p_shape, const Vector2 &p_offset, bool p_one_way = false);
-	int tile_get_shape_count(int p_id) const;
-
-	void tile_set_shapes(int p_id, const Vector<ShapeData> &p_shapes);
-	Vector<ShapeData> tile_get_shapes(int p_id) const;
+	void tile_set_shape(int p_id, const Ref<Shape2D> &p_shape);
+	Ref<Shape2D> tile_get_shape(int p_id) const;
 
 	void tile_set_material(int p_id, const Ref<ShaderMaterial> &p_material);
 	Ref<ShaderMaterial> tile_get_material(int p_id) const;
@@ -135,6 +114,9 @@ public:
 
 	void tile_set_navigation_polygon(int p_id, const Ref<NavigationPolygon> &p_navigation_polygon);
 	Ref<NavigationPolygon> tile_get_navigation_polygon(int p_id) const;
+
+	void tile_set_shapes(int p_id, const Vector<Ref<Shape2D> > &p_shapes);
+	Vector<Ref<Shape2D> > tile_get_shapes(int p_id) const;
 
 	void remove_tile(int p_id);
 
